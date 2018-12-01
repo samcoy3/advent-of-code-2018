1df5eb2b2b56d7371a8118008d8a0b482a122453
Sam Coy <samcoy3@gmail.com>
2018-12-01 10:07:33 +0000

    Added day 1 solutions

--- src/advent_of_code_2018/core.clj
+++ src/advent_of_code_2018/core.clj
@@ -1,6 +1,13 @@
 (ns advent-of-code-2018.core
+  (:use [clojure.pprint])
+  (:require [advent-of-code-2018.day01 :as day01])
   (:gen-class))
 
 (defn -main
   "Prints all of the solutions to the problems solved so far."
-  [])
+  []
+  (print-table
+   [
+    {:day 1 :part "A" :answer (day01/part-a)}
+    {:day 1 :part "B" :answer (day01/part-b)}
+    ]))
